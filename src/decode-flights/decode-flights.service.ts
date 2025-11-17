8b586b31df74550ce8277dae6b9aa68a896b08f5
gabrielolivra_ <gabernardinooliveira@gmail.com>
2025-11-17 17:23:42 -0300

    refactor: improve variable naming and structure in decodeFlights method

--- src/decode-flights/decode-flights.service.ts
+++ src/decode-flights/decode-flights.service.ts
@@ -1,22 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { gunzipSync } from 'zlib';
 import { DecodeFlightsFile } from './decode-flights.contract';
+import { json } from 'stream/consumers';
 
 @Injectable()
 export class DecodeFlightsService {
+
     async decodeFlights(data: DecodeFlightsFile): Promise<any> {
+        
         if(!data || !data.buffer) {
             throw new Error('Invalid file data');
         }
 
         const decodedData = Buffer.from(data.buffer, 'base64').toString('utf-8');
 
-        const json = JSON.parse(decodedData);
+        const jsonDecoded = JSON.parse(decodedData);
 
-        const base64Data = json.data.$binary.base64;
+        const base64Data = jsonDecoded.data.$binary.base64;
+
         const compressedBuffer = Buffer.from(base64Data, 'base64');
 
         const decompressedBuffer = gunzipSync(compressedBuffer);
+
         const decompressedData = decompressedBuffer.toString('utf-8');
 
         const flights = JSON.parse(decompressedData);
