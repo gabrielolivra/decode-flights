3de9c9d64d43bafe087d823b3aad0f29f8e372a6
gabrielolivra_ <gabernardinooliveira@gmail.com>
2025-11-17 17:29:29 -0300

    refactor: enhance error handling in decodeFlights method

--- src/decode-flights/decode-flights.service.ts
+++ src/decode-flights/decode-flights.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { gunzipSync } from 'zlib';
 import { DecodeFlightsFile } from './decode-flights.contract';
 import { json } from 'stream/consumers';
@@ -7,9 +7,9 @@ import { json } from 'stream/consumers';
 export class DecodeFlightsService {
 
     async decodeFlights(data: DecodeFlightsFile): Promise<any> {
-        
-        if(!data || !data.buffer) {
-            throw new Error('Invalid file data');
+
+        if(this.isNullOrEmpty(data) || this.isNullOrEmpty(data.buffer)) {
+            throw new BadRequestException('Invalid file data');
         }
 
         const decodedData = Buffer.from(data.buffer, 'base64').toString('utf-8');
@@ -27,5 +27,9 @@ export class DecodeFlightsService {
         const flights = JSON.parse(decompressedData);
 
         return { flights };
+    }
+
+    private isNullOrEmpty(value: any): boolean {
+        return value === null || value === undefined || value === '';
     }
 }
